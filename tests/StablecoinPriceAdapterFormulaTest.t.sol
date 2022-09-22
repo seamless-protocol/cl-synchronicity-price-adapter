0d468f012c017ccf260658c1ccdd8b2720323c25
dusan <duxxud@gmail.com>
2022-09-22 04:43:03 +0200

    feat: use foundry mocks instead of custom mock contract

--- tests/StablecoinPriceAdapterFormulaTest.t.sol
+++ tests/StablecoinPriceAdapterFormulaTest.t.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
-import "forge-std/console.sol";
 
 import {StablecoinPriceAdapter} from '../src/contracts/StablecoinPriceAdapter.sol';
 import {IChainlinkAggregator} from '../src/interfaces/IChainlinkAggregator.sol';
-import {MockAggregator} from './mock/MockAggregator.sol';
 
 contract StablecoinPriceAdapterFormulaTest is Test {
 
@@ -16,32 +14,34 @@ contract StablecoinPriceAdapterFormulaTest is Test {
   function setUp() public {}
 
   function testFormula() public {
-
     uint256 TESTS = 10;
   
     int256 ethPrice = ETH_USD_AGGREGATOR.latestAnswer();
-    
+    uint8 ethAggregatorDecimals = ETH_USD_AGGREGATOR.decimals();
+
     for (uint256 i = 1; i <= TESTS; i++) {
-      
-      MockAggregator aggregator = new MockAggregator(
-        ethPrice / int256(i), 
-        ETH_USD_AGGREGATOR.decimals()
-      ); 
+      address mockAggregator = address(0);
+      int256 mockPrice = ethPrice / int256(i);
+      _setMockPrice(mockAggregator, mockPrice);
 
       StablecoinPriceAdapter adapter = new StablecoinPriceAdapter(
         address(ETH_USD_AGGREGATOR),
-        address(aggregator)
+        mockAggregator
       );
 
       int256 price = adapter.latestAnswer();
       int256 expectedPriceInEth = int256(1 ether / i);
 
-      int256 maxDiff = int256(10 ** (aggregator.decimals()));
-      assertTrue(_abs(price - expectedPriceInEth) < maxDiff);
+      int256 maxDiff = int256(10 ** (ethAggregatorDecimals));
+      assertApproxEqAbs(uint256(price), uint256(expectedPriceInEth), uint256(maxDiff));
     }
   }
 
-  function _abs(int256 x) internal pure returns(int256) {
-    return (x > 0) ? x : -x;
+  function _setMockPrice(address mockAggregator, int256 mockPrice) internal {
+      bytes memory latestAnswerCall = abi.encodeWithSignature("latestAnswer()");
+      bytes memory decimalsCall = abi.encodeWithSignature("decimals()");
+
+      vm.mockCall(mockAggregator, latestAnswerCall, abi.encode(mockPrice));
+      vm.mockCall(mockAggregator, decimalsCall, abi.encode(8));
   }
 }
