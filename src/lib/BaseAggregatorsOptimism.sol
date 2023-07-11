b4d406b4992cedddc00a04eac69ed6d2f96246eb
efecarranza <fcarr004@fiu.edu>
2023-07-10 22:27:44 -0400

    feat: add optimism oracle

--- src/lib/BaseAggregatorsOptimism.sol
+++ src/lib/BaseAggregatorsOptimism.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.0;
 library BaseAggregatorsOptimism {
   address public constant ETH_USD_AGGREGATOR = 0x13e3Ee699D1909E989722E753853AE30b17e08c5;
   address public constant WSTETH_STETH_AGGREGATOR = 0xe59EBa0D492cA53C6f46015EEa00517F2707dc77;
+  address public constant RETH_ETH_AGGREGATOR = 0x22F3727be377781d1579B7C9222382b21c9d1a8f;
 }
