f3778bb27c2c48c402c2eeccc4433e8f7ba9501e
Harsh Pandey <harshsatishpandey@gmail.com>
2023-10-18 19:36:57 +0530

    test: sDAI gnosis chain adapter

---
+++ src/lib/BaseAggregatorsGnosis.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library BaseAggregatorsGnosis {
+  address public constant DAI_USD_AGGREGATOR = 0x678df3415fc31947dA4324eC63212874be5a82f8;
+  address public constant SDAI = 0xaf204776c7245bF4147c2612BF6e5972Ee483701;
+}
