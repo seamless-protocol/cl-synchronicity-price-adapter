5bc41f88a3db42e2faa7e07697962893968ec351
Harsh Pandey <harshsatishpandey@gmail.com>
2023-10-18 19:39:01 +0530

    chore: add deploy scripts

--- scripts/DeploysDAIAdapter.s.sol
+++ scripts/DeploysDAIAdapter.s.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import {Script} from 'forge-std/Script.sol';
 import {sDAISynchronicityPriceAdapter} from '../src/contracts/sDAISynchronicityPriceAdapter.sol';
-import {BaseAggregatorsMainnet} from '../src/lib/BaseAggregators.sol';
+import {sDAIGnosisChainSynchronicityPriceAdapter} from '../src/contracts/sDAIGnosisChainSynchronicityPriceAdapter.sol';
+import {BaseAggregatorsMainnet, BaseAggregatorsGnosis} from '../src/lib/BaseAggregators.sol';
 
 contract DeploysDAIMainnet is Script {
   function run() external {
@@ -12,6 +13,20 @@ contract DeploysDAIMainnet is Script {
     new sDAISynchronicityPriceAdapter(
       BaseAggregatorsMainnet.DAI_USD_AGGREGATOR,
       BaseAggregatorsMainnet.SDAI_POT,
+      'sDAI/DAI/USD'
+    );
+
+    vm.stopBroadcast();
+  }
+}
+
+contract DeploysDAIGnosis is Script {
+  function run() external {
+    vm.startBroadcast();
+
+    new sDAIGnosisChainSynchronicityPriceAdapter(
+      BaseAggregatorsGnosis.DAI_USD_AGGREGATOR,
+      BaseAggregatorsGnosis.SDAI,
       'sDAI/DAI/USD'
     );
 
